852f4f482cbbe45f6370f57b7c3cbc75c09d9e07
do708 <do@mdb-networks.nl>
2026-06-16 10:27:17 +0200

    Improved navbar U70

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function Home() {
 return ( <main className="min-h-screen bg-white">
 
-```
   {/* HERO */}
 
   <section className="bg-gradient-to-br from-blue-50 via-white to-purple-50 py-24">
